58d2d9e654f6ea78493a90be750ab9a9e4fed2b2
ckc <ckpapa@yahoo.com>
2021-04-29 23:29:04 +0800

    initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      list: list,
+      searchTerm: 'search..',
+    };
+  }
+
+  // JS ES6 arrow function autobinds automatically method to constructor
+  deleteListItem = (id) => {
+    console.log(id);
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    console.log(updatedList)
+    this.setState({list: updatedList});
+  }
+
+  filterList = (search) => {
+    console.log('search: ' +  search);
+
+    if (search == ''){
+      console.log('empty')
+      console.log(list)
+      this.setState({list}); // refresh
+    }
+    else {
+      let updatedList = [];
+      this.state.list.map(item => {
+        if (item.author.toLowerCase().includes(search.toLowerCase()) ||
+            item.title.toLowerCase().includes(search.toLowerCase())
+        ){
+          console.log('found')
+          updatedList.push(item);
+        }
+      });
+
+      console.log('updated: ' + updatedList)
+      this.setState({list: updatedList});
+    }
+  }
+
+  onSearchChange = (event) => {
+    console.log('event: ' + event.target.value)
+    this.setState({
+      searchTerm: event.target.value
+    }, ()=>{
+      this.filterList(this.state.searchTerm);
+    });
+  }
+  onSearchFocus = (event) => {
+    this.setState({
+      searchTerm: ''
+    });
+  }
+
+  render(){
+    const {list, searchTerm} = this.state;
+    return(
+      <div className='App'>
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+          onFocus={this.onSearchFocus}
+        />
+        <Table
+          list={list}
+          deleteListItem={this.deleteListItem}
+        />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange, onFocus} = this.props;
+    return (
+      <form>
+        <input
+        type="text"
+        value={value}
+        onChange={onChange}
+        onFocus={onFocus}
+        />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render(){
+    const {list, deleteListItem} = this.props;
+    return(
+      <div>
+      {list.map(item =>
+        <div key={item.objectID}><span><a href={item.url}>{item.title}</a></span>
+            <span>{item.author}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={()=>deleteListItem(item.objectID)}
+                type='button'
+              >Delete</button>
+            </span>
+        </div>
+      )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+//Hot Module Replacement (HMR) is a tool to reload your application in the browser. The browser
+//doesn’t perform a page refresh. You can easily activate it in create-react-app. In your src/index.js,
+//your entry point to React, you have to add one little configuration.
+
+if (module.hot) {
+  module.hot.accept();
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
